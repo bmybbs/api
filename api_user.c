c7e9073ace1395471723f21ff6440f59dac9c7c5
IronBlood <aplshenyang@gmail.com>
2013-10-21 00:39:49 +0800

    查询用户信息接口 api_user_query 部分实现

---
+++ api_user.c
@@ -0,0 +1,45 @@
+#include "api.h"
+
+int api_user_login(ONION_FUNC_PROTO_STR)
+{
+	return OCS_NOT_IMPLEMENTED;
+}
+
+int api_user_query(ONION_FUNC_PROTO_STR)
+{
+	const onion_dict *param_dict = onion_request_get_query_dict(req);
+	const char * userid = onion_dict_get(param_dict, "userid");
+	if(userid[0]=='\0') {
+		return api_error(p, req, res, API_RT_WRONGPARAM);
+	}
+
+	struct userec *ue;
+
+	ue = getuser(userid);
+	if(ue == 0) {
+		return api_error(p, req, res, API_RT_NOSUCHUSER);
+	}
+
+	api_template_t tpl = api_template_create("templates/api_user_info.json");
+	api_template_set(&tpl, "UserID", "%s", ue->userid);
+	api_template_set(&tpl, "UserNickName", "%s", ue->username);
+	api_template_set(&tpl, "LoginCounts", "%d", ue->numlogins);
+	api_template_set(&tpl, "PostCounts", "%d", ue->numposts);
+
+	onion_response_set_header(res, "Content-type", "application/json; charset=utf-8");
+	onion_response_printf(res, "%s", tpl);
+
+	api_template_free(tpl);
+
+	return OCS_PROCESSED;
+}
+
+int api_user_logout(ONION_FUNC_PROTO_STR)
+{
+	return OCS_NOT_IMPLEMENTED;
+}
+
+int api_user_check_session(ONION_FUNC_PROTO_STR)
+{
+	return OCS_NOT_IMPLEMENTED;
+}
