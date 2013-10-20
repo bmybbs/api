0b5a1ad3b382870ff301bb76e55f8358638dcdb6
IronBlood <aplshenyang@gmail.com>
2013-10-21 00:06:25 +0800

    状态码字段改为 errcode

--- api_error.c
+++ api_error.c
@@ -3,6 +3,6 @@
 int api_error(ONION_FUNC_PROTO_STR, enum api_error_code errcode)
 {
 	onion_response_set_header(res, "Content-type", "application/json; charset=utf-8");
-	onion_response_printf(res, "{\"error code\":%d}", errcode);
+	onion_response_printf(res, "{\"errcode\":%d}", errcode);
 	return OCS_PROCESSED;
 }
