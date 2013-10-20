0409fbc6ea3e0a167bd64057b938a1d90afc45af
IronBlood <aplshenyang@gmail.com>
2013-10-20 18:28:42 +0800

    api error output

--- api.h
+++ api.h
@@ -4,6 +4,7 @@
 #include "error_code.h"
 
 #include <onion/onion.h>
+#include <onion/dict.h>
 #include <onion/log.h>
 #include <signal.h>
 #include <netdb.h>
@@ -12,7 +13,7 @@ onion *o=NULL;
 
 #define ONION_FUNC_PROTO_STR void *p, onion_request *req, onion_response *res
 
-int api_error(ONION_FUNC_PROTO_STR);
+int api_error(ONION_FUNC_PROTO_STR, enum api_error_code errcode);
 
 int api_user_login(ONION_FUNC_PROTO_STR);
 int api_user_query(ONION_FUNC_PROTO_STR);
---
+++ api_error.c
@@ -0,0 +1,8 @@
+#include "api.h"
+
+int api_error(ONION_FUNC_PROTO_STR, enum api_error_code errcode)
+{
+	onion_response_set_header(res, "Content-type", "application/json; charset=utf-8");
+	onion_response_printf(res, "{\"error code\":%d}", errcode);
+	return OCS_PROCESSED;
+}
