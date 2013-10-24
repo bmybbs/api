96fa286bfa0ed254e5af85995bfaaaa47ed7e786
IronBlood <aplshenyang@gmail.com>
2013-10-25 00:38:52 +0800

    新增 API_RT_NOEMTSESS 错误码，用于标识系统用户已满

--- error_code.h
+++ error_code.h
@@ -3,6 +3,7 @@
 
 enum api_error_code {
 	API_RT_SUCCESSFUL 	= 0, 		///< 成功调用
+	API_RT_NOEMTSESS	= 1,		///< 系统用户已满
 	API_RT_WRONGPARAM	= 1000,		///< 接口参数错误
 	API_RT_NOTEMPLATE	= 1100,		///< 没有模板
 	API_RT_NOSUCHUSER 	= 100000, 	///< 没有此用户
