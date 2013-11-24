3ba0d1519e92bea5e9ca670055d5b3550f3a6013
IronBlood <aplshenyang@gmail.com>
2013-11-24 20:04:28 +0800

    新增一些和发帖有关的错误码

--- error_code.h
+++ error_code.h
@@ -11,10 +11,12 @@ enum api_error_code {
 	API_RT_NOCMMNDFILE	= 6,		///< 没有找到美文、通知的文件
 	API_RT_NOGDBRDFILE	= 7,		///< 没有用户的收藏夹文件
 	API_RT_CNTMAPBRDIR	= 8,		///< 不能 MMAP 版面 .DIR 文件
+	API_RT_ATCLINNERR	= 9,		///< 发文遇到内部错误
 	API_RT_WRONGPARAM	= 1000,		///< 接口参数错误
 	API_RT_WRONGSESS	= 1001,		///< 错误的session
 	API_RT_NOTLOGGEDIN	= 1002,		///< 没有登录
 	API_RT_FUNCNOTIMPL	= 1003,		///< 功能未实现
+	API_RT_WRONGMETHOD	= 1004,		///< 错误的 HTTP 方法
 	API_RT_NOTEMPLATE	= 1100,		///< 没有模板
 	API_RT_NOSUCHUSER 	= 100000, 	///< 没有此用户
 	API_RT_SITEFBDIP	= 100001,	///< 站点禁用IP
@@ -22,11 +24,16 @@ enum api_error_code {
 	API_RT_ERRORPWD		= 100003,	///< 用户密码错误
 	API_RT_FBDNUSER		= 100004,	///< 用户没有登录权限
 	API_RT_INVSESSID	= 100005,	///< 用户session已失效
+	API_RT_WRONGTOKEN	= 100006,	///< 错误的用户 token
 	API_RT_NOSUCHBRD	= 110000,	///< 没有此版面
 	API_RT_NOBRDRPERM	= 110001,	///< 没有该版面的阅读权限
 	API_RT_EMPTYBRD		= 110002,	///< 版面没有文章
+	API_RT_NOBRDPPERM	= 110003,	///< 没有该版面的发文权限
+	API_RT_FBDGSTPIP	= 110004,	///< 禁止 guest 发帖 IP
 	API_RT_NOSUCHATCL	= 120000,	///< 没有这篇文章
 	API_RT_ATCLDELETED	= 120001,	///< 文章已被删除
+	API_RT_ATCLNOTITLE	= 120002,	///< 文章缺少标题
+	API_RT_ATCLFBDREPLY	= 120003,	///< 文章禁止回复
 };
 
 #endif
