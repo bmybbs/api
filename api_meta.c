6dc51fd6abbbaeac00f27b6e41999aec2e8793fb
IronBlood <aplshenyang@gmail.com>
2014-11-25 22:23:32 +0800

    更新获取进站画面的接口调用
    需要使用最新的 feature/bmyapp 分支

--- api_meta.c
+++ api_meta.c
@@ -2,11 +2,11 @@
 
 int api_meta_loginpics(ONION_FUNC_PROTO_STR)
 {
-	char pics[512];
-	get_no_more_than_four_login_pics(pics, 512);
+	char *pics = get_no_more_than_four_login_pics();
 
 	api_set_json_header(res);
 	onion_response_printf(res, "{\"errcode\":0, \"pics\":\"%s\"}", pics);
 
+	free(pics);
 	return OCS_PROCESSED;
 }
