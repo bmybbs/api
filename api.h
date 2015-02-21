918db456aaf6a75707c61e27fe93d709095a5856
IronBlood <aplshenyang@gmail.com>
2015-02-21 22:52:13 +0800

    上传附件接口声明

--- api.h
+++ api.h
@@ -57,6 +57,7 @@ int api_meta_loginpics(ONION_FUNC_PROTO_STR);			// 进站画面
 
 int api_attach_show(ONION_FUNC_PROTO_STR);				// 显示附件
 int api_attach_list(ONION_FUNC_PROTO_STR);				// 附件列表
+int api_attach_upload(ONION_FUNC_PROTO_STR);				// 上传附件
 
 /**
  * @brief 为 onion_response 添加 json 的 MIME 信息
