b0a2407eeebd69ff79c59506763a533886b2e73a
IronBlood <aplshenyang@gmail.com>
2015-03-11 10:18:47 +0800

    获取所有提醒的接口实现

--- api_notification.c
+++ api_notification.c
@@ -2,7 +2,49 @@
 
 int api_notification_list(ONION_FUNC_PROTO_STR)
 {
-	return OCS_NOT_PROCESSED;
+	const char * userid = onion_request_get_query(req, "userid");
+	const char * appkey = onion_request_get_query(req, "appkey");
+	const char * sessid = onion_request_get_query(req, "sessid");
+
+	if (userid == NULL || sessid == NULL || appkey == NULL) {
+		return api_error(p, req, res, API_RT_WRONGPARAM);
+	}
+
+	struct userec *ue = getuser(userid);
+	if (ue == 0) {
+		return api_error(p, req, res, API_RT_NOSUCHUSER);
+	}
+
+	int r = check_user_session(ue, sessid, appkey);
+	if (r != API_RT_SUCCESSFUL) {
+		free(ue);
+		return api_error(p, req, res, r);
+	}
+
+	struct json_object *obj = json_tokener_parse("{\"errcode\": 0, \"notifications\": []}");
+	struct json_object *noti_array = json_object_object_get(obj, "notifications");
+	NotifyItemList allNotifyItems = parse_notification(ue->userid);
+	struct json_object * item = NULL;
+	struct NotifyItem * currItem;
+	for (currItem = (struct NotifyItem *)allNotifyItems; currItem != NULL; currItem = currItem->next) {
+		item = json_object_new_object();
+		json_object_object_add(item, "board", json_object_new_string(currItem->board));
+		json_object_object_add(item, "noti_time", json_object_new_int64(currItem->noti_time));
+		json_object_object_add(item, "from_userid", json_object_new_string(currItem->from_userid));
+		json_object_object_add(item, "title", json_object_new_string(currItem->title_utf));
+		json_object_object_add(item, "type", json_object_new_int(currItem->type));
+
+		json_object_array_add(noti_array, item);
+	}
+
+	free_notification(allNotifyItems);
+
+	api_set_json_header(res);
+	onion_response_write0(res, json_object_to_json_string(obj));
+	json_object_put(obj);
+	free(ue);
+
+	return OCS_PROCESSED;
 }
 
 int api_notification_del(ONION_FUNC_PROTO_STR)
