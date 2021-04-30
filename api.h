1f619cbd0ca96b8c4dd9b7863cd3ad0f13bc6f6d
IronBlood <aplshenyang@gmail.com>
2021-04-30 22:55:17 +0800

    新增 search/content 接口

--- api.h
+++ api.h
@@ -63,6 +63,8 @@ int api_notification_del(ONION_FUNC_PROTO_STR);
 
 int api_subscription_list(ONION_FUNC_PROTO_STR);
 
+int api_search_content(ONION_FUNC_PROTO_STR);
+
 /**
  * @brief 为 onion_response 添加 json 的 MIME 信息
  * @param res
